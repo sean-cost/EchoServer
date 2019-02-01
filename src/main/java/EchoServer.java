8e851ca80a756d229cf9402e583bcc6be2b0ee7c
Merce Bauza <merce.bauza@gmail.com>
2019-02-01 10:01:00 +0000

    adds readme

--- src/main/java/EchoServer.java
+++ src/main/java/EchoServer.java
@@ -6,8 +6,9 @@ public class EchoServer {
     public static void main(String[] args) throws IOException {
         int portNumber = Integer.parseInt(args[0]);
         ServerSocket ss = new ServerSocket(portNumber);
+        UserInterface ui = new UserInterface(System.out);
 
-        Chatterbox server = new Chatterbox(ss, new UserInterface(System.out));
+        Chatterbox server = new Chatterbox(ss, ui);
         server.start();
     }
 }
